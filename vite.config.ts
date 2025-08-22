f9f7f768853594f9eebda916cf03a93135223800
Nikos Paraskevopoulos <nikospara@yahoo.com>
2025-08-23 01:18:17 +0300

    Build switches for Chrome/Firefox and the option to watch

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,37 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 /**
  * @see https://vitejs.dev/config/
  */
-export default defineConfig({
-	plugins: [tsconfigPaths(), react(), eslintPlugin()],
-	resolve: {
-		alias: {
-			'@': path.resolve('./src'),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	const browser = (env.VITE_TARGET_BROWSER || 'Chrome').toLowerCase();
+
+	const viteStaticCopyCfg = viteStaticCopy({
+		targets: [
+			{
+				src: `public-${browser}/manifest.json`,
+				dest: '',
+				overwrite: true,
+			},
+		],
+	});
+
+	return {
+		plugins: [tsconfigPaths(), react(), eslintPlugin(), viteStaticCopyCfg],
+		resolve: {
+			alias: {
+				'@': path.resolve('./src'),
+			},
 		},
-	},
-	build: {
-		// Set to {} to enable a watched build workflow, null to disable (default).
-		// If only it could be defined in the command line.
-		watch: null,
-	},
+		build: {
+			// Set to {} to enable a watched build workflow, null to disable (default).
+			watch: env.VITE_WATCH ? {} : null,
+		},
+	};
 });
