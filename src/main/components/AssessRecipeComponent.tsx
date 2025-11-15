6350005042d4f24b9b4feffe6cdb1852bbcff888
Nikos Paraskevopoulos <nikospara@yahoo.com>
2025-11-15 21:09:40 +0200

    Indicator to show when the assessed recipe is not of this page

--- src/main/components/AssessRecipeComponent.tsx
+++ src/main/components/AssessRecipeComponent.tsx
@@ -1,6 +1,7 @@
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { GrUndo } from 'react-icons/gr';
-import { TbWorldUpload } from 'react-icons/tb';
+import { TbAlertTriangleFilled, TbWorldUpload } from 'react-icons/tb';
 
 export interface AssessRecipeComponentProps {
 	assessing: boolean;
@@ -13,6 +14,72 @@ export interface AssessRecipeComponentProps {
 const AssessRecipeComponent: React.FC<AssessRecipeComponentProps> = (
 	props: AssessRecipeComponentProps,
 ) => {
+	const [assessedTabUrl, setAssessedTabUrl] = useState<
+		string | null | undefined
+	>(null);
+	const activeTabId = useRef<number | null | undefined>(-1);
+
+	useEffect(() => {
+		if (
+			typeof browser !== 'undefined' &&
+			typeof browser.tabs !== 'undefined'
+		) {
+			const activatedListener = (
+				activeInfo: browser.tabs._OnActivatedActiveInfo,
+			) => {
+				activeTabId.current = activeInfo.tabId;
+				browser.tabs
+					.get(activeInfo.tabId)
+					.then((tab) => setAssessedTabUrl(tab.url));
+			};
+			browser.tabs.onActivated.addListener(activatedListener);
+
+			const updatedListener = (
+				tabId: number,
+				changeInfo: browser.tabs._OnUpdatedChangeInfo,
+			) => {
+				if (tabId === activeTabId.current && changeInfo.url) {
+					setAssessedTabUrl(changeInfo.url);
+				}
+			};
+			browser.tabs.onUpdated.addListener(updatedListener);
+
+			return () => {
+				browser.tabs.onActivated.removeListener(activatedListener);
+				browser.tabs.onUpdated.removeListener(updatedListener);
+			};
+		} else if (
+			typeof chrome !== 'undefined' &&
+			typeof chrome.tabs !== 'undefined'
+		) {
+			const activatedListener = (
+				activeInfo: chrome.tabs.OnActivatedInfo,
+			) => {
+				activeTabId.current = activeInfo.tabId;
+				chrome.tabs.get(activeInfo.tabId, (tab) => {
+					console.log('ASSESSED URL', tab.url);
+					setAssessedTabUrl(tab.url);
+				});
+			};
+			chrome.tabs.onActivated.addListener(activatedListener);
+
+			const updatedListener = (
+				tabId: number,
+				changeInfo: chrome.tabs.OnUpdatedInfo,
+			) => {
+				if (tabId === activeTabId.current && changeInfo.url) {
+					setAssessedTabUrl(changeInfo.url);
+				}
+			};
+			chrome.tabs.onUpdated.addListener(updatedListener);
+
+			return () => {
+				chrome.tabs.onActivated.removeListener(activatedListener);
+				chrome.tabs.onUpdated.removeListener(updatedListener);
+			};
+		}
+	}, []);
+
 	const { t } = useTranslation();
 
 	return (
@@ -53,6 +120,21 @@ const AssessRecipeComponent: React.FC<AssessRecipeComponentProps> = (
 							? t('main.AssessRecipeComponent.assessing')
 							: t('main.AssessRecipeComponent.assessed')}
 					</span>
+				) : null}
+				{!!props.url &&
+				assessedTabUrl &&
+				props.url !== assessedTabUrl ? (
+					<>
+						&nbsp;
+						<span>
+							<TbAlertTriangleFilled
+								className="text-(--color-error) inline-block"
+								title={t(
+									'main.AssessRecipeComponent.notTheAssessedPage',
+								)}
+							/>
+						</span>
+					</>
 				) : null}
 				&nbsp;
 				{props.url || ''}
