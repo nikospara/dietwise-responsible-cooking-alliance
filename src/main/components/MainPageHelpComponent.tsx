6ba559f0dca1f84f4c0db4283372bf131d211770
Nikos Paraskevopoulos <nikospara@yahoo.com>
2026-04-22 21:35:23 +0300

    The content of the main page

--- src/main/components/MainPageHelpComponent.tsx
+++ src/main/components/MainPageHelpComponent.tsx
@@ -6,8 +6,24 @@ const MainPageHelpComponent: React.FC = () => {
 	return (
 		<div>
 			<h2>{t('main.MainPageHelpComponent.title')}</h2>
-			<p>{t('main.MainPageHelpComponent.paragraph1')}</p>
-			<p>{t('main.MainPageHelpComponent.paragraph2')}</p>
+			<p className="text-justify">{t('main.MainPageHelpComponent.welcome1')}</p>
+			<p className="text-justify">{t('main.MainPageHelpComponent.welcome2')}</p>
+			<hr />
+			<h3>{t('main.MainPageHelpComponent.titleInstructions')}</h3>
+			<p className="text-justify">{t('main.MainPageHelpComponent.paragraph1')}</p>
+			<p className="text-justify">{t('main.MainPageHelpComponent.paragraph2')}</p>
+			<p className="text-justify">{t('main.MainPageHelpComponent.paragraph3')}</p>
+			<ol className="list-decimal p-[20px]">
+				<li>{t('main.MainPageHelpComponent.explainUndecidedButton')}</li>
+				<li>
+					{t('main.MainPageHelpComponent.explainApproveButton')}: {t('main.SuggestionsComponent.accept')}
+				</li>
+				<li>
+					{t('main.MainPageHelpComponent.explainRejectButton')}: {t('main.SuggestionsComponent.reject')}
+				</li>
+			</ol>
+			<p className="text-justify">{t('main.MainPageHelpComponent.paragraph4')}</p>
+			<p className="text-justify">{t('main.MainPageHelpComponent.paragraph5')}</p>
 		</div>
 	);
 };
